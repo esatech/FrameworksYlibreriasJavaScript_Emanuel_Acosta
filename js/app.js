baf594f972756fe651b3ba7c7586af599cf687dc
Emanuel <emanuel.j.acosta@gmail.com>
2019-03-23 18:32:36 -0300

    inicio juego

--- js/app.js
+++ js/app.js
@@ -1,21 +1,37 @@
-function tituloRojo(){
-  $(".main-titulo").animate(
-    {
-      color: "red"
-    }, 1000, function(){
-      tituloAzul()
-    }
-  )
-}
+$( document ).ready(function() {
+  //Funcion para cambiar color de titulo
+  function tituloRojo(){
+    $(".main-titulo").animate(
+      {
+        color: "red"
+      }, 1000, function(){
+        tituloAzul()
+      }
+    )
+  }
 
-function tituloAzul(){
-  $(".main-titulo").animate(
-    {
-      color: "blue"
-    }, 1000, function(){
-      tituloRojo()
-    }
-  )
-}
+  function tituloAzul(){
+    $(".main-titulo").animate(
+      {
+        color: "blue"
+      }, 1000, function(){
+        tituloRojo()
+      }
+    )
+  }
 
-tituloAzul()
+  tituloAzul()
+
+  //inicia el juego y agrega caramelos aleatoreos
+  $(".btn-reinicio").click(function(){
+    $(".col-1").append("<img src='image/1.png' class='elemento'>").setInterval(500);
+    $(".col-1").append("<img src='image/2.png' class='elemento'>").setInterval(500);
+    $(".col-1").append("<img src='image/3.png' class='elemento'>").setInterval(500);
+    $(".col-1").append("<img src='image/4.png' class='elemento'>").setInterval(1000);
+    $(".col-1").append("<img src='image/1.png' class='elemento'>").setInterval(1000);
+    $(".col-1").append("<img src='image/3.png' class='elemento'>").setInterval(1000);
+
+  });
+
+
+});
