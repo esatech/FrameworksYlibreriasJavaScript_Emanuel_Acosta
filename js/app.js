22686b6e879c8a00c413079225827205cd6005ea
Emanuel <emanuel.j.acosta@gmail.com>
2019-03-30 12:26:27 -0300

    temperizador

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+var i=0;
+var intervalo=0;
+var tiempo=0;
+var min=2;
+var seg=0;
+
+
 $( document ).ready(function() {
   //Funcion para cambiar color de titulo
   function tituloRojo(){
@@ -24,14 +31,47 @@ $( document ).ready(function() {
 
   //inicia el juego y agrega caramelos aleatoreos
   $(".btn-reinicio").click(function(){
-    $(".col-1").append("<img src='image/1.png' class='elemento'>").setInterval(500);
-    $(".col-1").append("<img src='image/2.png' class='elemento'>").setInterval(500);
-    $(".col-1").append("<img src='image/3.png' class='elemento'>").setInterval(500);
-    $(".col-1").append("<img src='image/4.png' class='elemento'>").setInterval(1000);
-    $(".col-1").append("<img src='image/1.png' class='elemento'>").setInterval(1000);
-    $(".col-1").append("<img src='image/3.png' class='elemento'>").setInterval(1000);
+    intervalo=setInterval(function(){
+      desplazamiento()
+    },600);
+    $(this).html("Reiniciar")
+    tiempo=setInterval(function(){
+  		timer()
+  	},1000);
 
   });
 
+  function desplazamiento(){
+    i=i+1
+    var numero=0;
+    var imagen=0;
+    if (i<8) {
+      for (var j = 1; j < 8; j++) {
+        numero=Math.floor(Math.random()*4)+1;
+        imagen="image/"+numero+".png";
+        $(".col-"+j).prepend("<img src="+imagen+" class='elemento'>").css("justify-content","flex-start")
+      }
+    }
+    if (i==8) {
+      clearInterval(intervalo);
+    }
+  }
+
+  // temporizador hacia atrás
+  function timer(){
+  	if(seg!=0){
+  		seg=seg-1;}
+  	if(seg==0){
+  		if(min==0){
+  			//clearInterval(eliminar);
+  			//clearInterval(nuevosDulces);
+  			clearInterval(intervalo);
+  			clearInterval(tiempo);
+  			$(".panel-tablero").hide("drop","slow",funcioncita);
+  			$(".time").hide();}
+  		seg=59;
+  		min=min-1;}
+  	$("#timer").html("0"+min+":"+seg);
+  };
 
 });
